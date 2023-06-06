a9937d4098707bf1fc9c4f2b54660d4b3ba223ed
Caroline Chan <carolinechan304@gmail.com>
2023-06-06 21:08:16 +0700

    CREATE: splashscreen view

--- UpgradedMovieApp/InitializedApp/SplashScreenView.swift
+++ UpgradedMovieApp/InitializedApp/SplashScreenView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct SplashScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(hex: "242A32")
+                .ignoresSafeArea()
+            
+            Image("Logo")
+        }
     }
 }
 
