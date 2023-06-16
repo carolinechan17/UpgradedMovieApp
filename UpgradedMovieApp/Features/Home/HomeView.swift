5a12ea2930d9bf9949ceacc273bd38520dbf8448
Caroline Chan <carolinechan304@gmail.com>
2023-06-16 21:41:54 +0700

    ADD: tab view

--- UpgradedMovieApp/Features/Home/HomeView.swift
+++ UpgradedMovieApp/Features/Home/HomeView.swift
@@ -28,13 +28,43 @@ struct HomeView: View {
                     ScrollableTabBarView(items: ["Top Movies", "Top TV Shows", "Popular Movies", "Popular TV Shows", "Now Playing", "Coming Soon", "Box Office", "Box Office All Time"], selectedIndex: $selectedIndex)
                         .padding(.vertical)
                     
+                    TabView(selection: $selectedIndex) {
+                        Image(systemName: "house")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .tag(0)
+                        
+                        Text("top tv show")
+                            .foregroundColor(.white)
+                            .tag(1)
+                        
+                        Text("popular movie")
+                            .foregroundColor(.white)
+                            .tag(2)
+                        Text("popular tv show")
+                            .foregroundColor(.white)
+                            .tag(3)
+                        Text("now playing")
+                            .foregroundColor(.white)
+                            .tag(4)
+                        Text("coming soon")
+                            .foregroundColor(.white)
+                            .tag(5)
+                        Text("box office")
+                            .foregroundColor(.white)
+                            .tag(6)
+                        Text("box office all time")
+                            .foregroundColor(.white)
+                            .tag(7)
+                    }
+                    .padding(.vertical)
+                    .onAppear {
+                        UITabBar.appearance().isHidden = true
+                    }
+                    
                     Spacer()
                 }
                 .padding()
-                .task {
-                    await homeVM.getTopMovies()
-                    await homeVM.getTopTVShows()
-                }
             }
         }
     }
