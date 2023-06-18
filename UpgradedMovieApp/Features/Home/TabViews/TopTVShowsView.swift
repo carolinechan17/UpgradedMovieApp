4ae0367d777630d39915e41132408c0583a13924
Caroline Chan <carolinechan304@gmail.com>
2023-06-18 11:23:26 +0700

    CREATE: top tv shows view

---
+++ UpgradedMovieApp/Features/Home/TabViews/TopTVShowsView.swift
@@ -0,0 +1,20 @@
+//
+//  TopTVShowsView.swift
+//  UpgradedMovieApp
+//
+//  Created by Caroline Chan on 18/06/23.
+//
+
+import SwiftUI
+
+struct TopTVShowsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopTVShowsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopTVShowsView()
+    }
+}
