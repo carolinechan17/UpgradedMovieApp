45d00c82b149892a905a8bd02048ae7d55ccc131
Caroline Chan <carolinechan304@gmail.com>
2023-06-18 11:37:26 +0700

    CREATE: new popular movies view

---
+++ UpgradedMovieApp/Features/Home/TabViews/PopularMoviesView.swift
@@ -0,0 +1,20 @@
+//
+//  PopularMoviesView.swift
+//  UpgradedMovieApp
+//
+//  Created by Caroline Chan on 18/06/23.
+//
+
+import SwiftUI
+
+struct PopularMoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopularMoviesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopularMoviesView()
+    }
+}
