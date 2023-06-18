be63fafda30ba978b588ec2a321d128f1c4c734e
Caroline Chan <carolinechan304@gmail.com>
2023-06-18 11:20:54 +0700

    ADD: top movies view to tab view

--- UpgradedMovieApp/Features/Home/HomeView.swift
+++ UpgradedMovieApp/Features/Home/HomeView.swift
@@ -16,56 +16,34 @@ struct HomeView: View {
             Color(hex: "242A32")
                 .ignoresSafeArea()
             
-            ScrollView(showsIndicators: false) {
-                VStack(alignment: .leading) {
-                    Text("What do you want to watch?")
-                        .font(.system(size: 18))
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
+            VStack(alignment: .leading) {
+                Text("What do you want to watch?")
+                    .font(.system(size: 18))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                
+                SearchBarView()
+                
+                ScrollableTabBarView(items: ["Top Movies", "Top TV Shows", "Popular Movies", "Popular TV Shows", "Now Playing", "Coming Soon", "Box Office", "Box Office All Time"], selectedIndex: $selectedIndex)
+                    .padding(.top)
+                
+                TabView(selection: $selectedIndex) {
+                    TopMoviesView()
+                        .tag(0)
+
+                    TopMoviesView()
+                        .tag(1)
                     
-                    SearchBarView()
-                    
-                    ScrollableTabBarView(items: ["Top Movies", "Top TV Shows", "Popular Movies", "Popular TV Shows", "Now Playing", "Coming Soon", "Box Office", "Box Office All Time"], selectedIndex: $selectedIndex)
-                        .padding(.vertical)
-                    
-                    TabView(selection: $selectedIndex) {
-                        Image(systemName: "house")
-                            .resizable()
-                            .frame(width: 100, height: 100)
-                            .tag(0)
-                        
-                        Text("top tv show")
-                            .foregroundColor(.white)
-                            .tag(1)
-                        
-                        Text("popular movie")
-                            .foregroundColor(.white)
-                            .tag(2)
-                        Text("popular tv show")
-                            .foregroundColor(.white)
-                            .tag(3)
-                        Text("now playing")
-                            .foregroundColor(.white)
-                            .tag(4)
-                        Text("coming soon")
-                            .foregroundColor(.white)
-                            .tag(5)
-                        Text("box office")
-                            .foregroundColor(.white)
-                            .tag(6)
-                        Text("box office all time")
-                            .foregroundColor(.white)
-                            .tag(7)
-                    }
-                    .padding(.vertical)
-                    .onAppear {
-                        UITabBar.appearance().isHidden = true
-                    }
-                    
-                    Spacer()
+                    TopMoviesView()
+                        .tag(2)
                 }
-                .padding()
+                .onAppear {
+                    UITabBar.appearance().isHidden = true
+                }
+                
+                Spacer()
             }
+            .padding()
         }
     }
 }
--- UpgradedMovieApp/Features/Home/TabViews/TopMoviesView.swift
+++ UpgradedMovieApp/Features/Home/TabViews/TopMoviesView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct TopMoviesView: View {
+    @StateObject private var homeVM: HomeViewModel = HomeViewModel()
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(hex: "242A32")
+                .ignoresSafeArea()
+            
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(homeVM.topMovies.items, id: \.self) { item in
+                        ZStack {
+                            ProgressView()
+                            
+                            AsyncImage(url: URL(string: item.image)) { image in
+                                image
+                                    .resizable()
+                                    .frame(width: 110, height: 150)
+                                    .scaledToFit()
+                            } placeholder: {
+                                Color.clear
+                            }
+                        }
+                        .frame(width: 110, height: 150)
+                        .cornerRadius(10)
+                    }
+                }
+                .padding(.vertical)
+            }
+            .task {
+                await homeVM.getTopMovies()
+            }
+        }
     }
 }
 
