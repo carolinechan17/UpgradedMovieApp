569c792c65046a12145238dde753312100de2915
Caroline Chan <carolinechan304@gmail.com>
2023-06-18 11:20:43 +0700

    CREATE: top movies view

---
+++ UpgradedMovieApp/Features/Home/TabViews/TopMoviesView.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoviesView.swift
+//  UpgradedMovieApp
+//
+//  Created by Caroline Chan on 17/06/23.
+//
+
+import SwiftUI
+
+struct TopMoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopMoviesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviesView()
+    }
+}
