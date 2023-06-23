aca80f8c74b5d86013fa4f3ad94dbd4b8b7371fa
Caroline Chan <carolinechan304@gmail.com>
2023-06-23 20:33:35 +0700

    ADD: navigation manager

---
+++ UpgradedMovieApp/Extension/NavigationManager.swift
@@ -0,0 +1,31 @@
+//
+//  NavigationManager.swift
+//  UpgradedMovieApp
+//
+//  Created by Caroline Chan on 23/06/23.
+//
+
+import Foundation
+import SwiftUI
+
+class NavigationManager: ObservableObject {
+    @Published var path: NavigationPath = NavigationPath()
+    
+    func goToRoot() {
+        guard !path.isEmpty else { return }
+        path.removeLast(path.count)
+    }
+    
+    func navigateTo(destination: Route) {
+        path.append(destination)
+    }
+    
+    func backToPrevious() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+}
+
+enum Route: Hashable {
+    case detailView
+}
