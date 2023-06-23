0a970b272df602242678d849d2c8aebc2173db85
Caroline Chan <carolinechan304@gmail.com>
2023-06-23 20:33:52 +0700

    CREATE: initialize detail view

---
+++ UpgradedMovieApp/Features/Detail/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  UpgradedMovieApp
+//
+//  Created by Caroline Chan on 23/06/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @EnvironmentObject var navigationManager: NavigationManager
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
