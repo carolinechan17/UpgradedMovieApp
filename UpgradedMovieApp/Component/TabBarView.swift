f1df4a550a38a9a85cdf24c1f57110573a98c29e
Caroline Chan <carolinechan304@gmail.com>
2023-06-24 13:42:51 +0700

    CREATE: tab bar view

---
+++ UpgradedMovieApp/Component/TabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarView.swift
+//  UpgradedMovieApp
+//
+//  Created by Caroline Chan on 24/06/23.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
